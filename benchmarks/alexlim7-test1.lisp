2b5801ef6f2fa6655ad29feacd644a77706d2b6e
alexlim7 <123590893+alexlim7@users.noreply.github.com>
2025-12-03 12:05:24 -0500

    Update alexlim7-test1.lisp

--- benchmarks/alexlim7-test1.lisp
+++ benchmarks/alexlim7-test1.lisp
@@ -1,9 +1,25 @@
-(define (step acc n)
-  (+ acc n))
+(define (add3 x)
+  (+ x 3))
 
-(define (loop n acc)
-  (if (= n 0)
-      acc
-      (loop (sub1 n) (step acc n))))
+(define (add5 x)
+  (+ x 5))
 
-(print (loop 1000 0))
+(define (id x)
+  x)
+
+(define (choose-const b)
+  (if b
+      10
+      20))
+
+(define (main)
+  (let ((a 7))
+    (let ((b (add3 a)))
+      (let ((c (add5 b)))
+        (let ((d (id c)))
+          (let ((flag (= 1 0)))
+            (if flag
+                (choose-const true)
+                (+ d 5))))))))
+
+(print (main))
