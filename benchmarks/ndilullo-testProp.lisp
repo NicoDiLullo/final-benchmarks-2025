0d174149b65f7a227bbb9d687566e42b498fb39a
NicoDiLullo <ndilullo@thacher.org>
2025-12-05 17:40:57 -0500

    fun formatting

--- benchmarks/ndilullo-testProp.lisp
+++ benchmarks/ndilullo-testProp.lisp
@@ -1,5 +1,18 @@
-(define (number42) (+ 7 (+ 7 (+ 7 (+ 7 (+ 7 (+ 14 -7)))))))
+(define (number42)
+    (+ 7 
+        (+ 7 
+            (+ 7 
+                (+ 7 
+                    (+ 7 
+                        (+ 14 -7)))))))
 
-(define (number42v2) (+ 7 (+ 7 (+ (let (x (+ 5 2)) x) (+ 7 (+ 7 (+ 14 (let (y (- 3 10) y)))))))))
+(define (number42v2)
+    (+ 7 
+        (+ 7 
+            (+ (let (x (+ 5 2)) x) 
+                (+ 7 
+                    (+ 7 
+                        (+ 14
+                            (let (y (- 3 10) y)))))))))
 
 (let (x (- (number42) (number42v2))) print(x))
