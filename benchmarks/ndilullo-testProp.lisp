2bc725f2896c255e53bd5a3959f4c52915912899
NicoDiLullo <ndilullo@thacher.org>
2025-12-05 18:35:50 -0500

    fix test prop on my hw6 compiler (compiles)

--- benchmarks/ndilullo-testProp.lisp
+++ benchmarks/ndilullo-testProp.lisp
@@ -1,18 +1,20 @@
 (define (number42)
-    (+ 7 
+  (+ 7 
+     (+ 7 
         (+ 7 
-            (+ 7 
-                (+ 7 
-                    (+ 7 
-                        (+ 14 -7)))))))
+           (+ 7 
+              (+ 7 
+                 (+ 14 -7)))))))
 
 (define (number42v2)
     (+ 7 
         (+ 7 
-            (+ (let (x (+ 5 2)) x) 
+            (+ (let ((x (+ 5 2))) x)
                 (+ 7 
                     (+ 7 
                         (+ 14
-                            (let (y (- 3 10) y)))))))))
+                            (let ((y (- 3 10))) y))))))))
 
-(let (x (- (number42) (number42v2))) print(x))
+(do
+    (let ((number (- (number42) (number42v2))))
+    (print number)))
